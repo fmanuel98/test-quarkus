eed10248f6e6dc2f7c76d94f19c21d64fdf05eb9
quarkusio <no-reply@quarkus.io>
2025-06-09 10:53:56 +0000

    Initial commit

---
+++ src/test/java/fmanuel98/com/github/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package fmanuel98.com.github;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
